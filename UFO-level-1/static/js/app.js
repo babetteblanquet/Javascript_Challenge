902ebd089046f85ec64417e7b7af335012b20138
babetteblanquet <65524644+babetteblanquet@users.noreply.github.com>
2020-08-24 10:52:27 +1000

    App.js updated

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -21,7 +21,7 @@ tableData.forEach((ufoData) => {
 var button = d3.select("#filter-btn");
 
 // Select the form
-var form = d3.select(".form-group");
+var form = d3.select("form");
 
 // Create event handlers 
 button.on("click", runEnter);
@@ -30,19 +30,25 @@ form.on("submit", runEnter);
 // Complete the event handler function for the form
 function runEnter() {
 
+  // Remove any ufo from the variable tbody 
+  tbody.html("");
+
   // Prevent the page from refreshing
   d3.event.preventDefault();
   
   // Select the input element and get the raw HTML node
-  var inputElement = d3.select("#datetime");
+  var inputElement = d3.select("#datetime.form-control");
 
   // Get the value property of the input element
   var inputValue = inputElement.property("value");
 
+  // Console log the input value and the tableData
   console.log(inputValue);
   console.log(tableData);
 
+  //Filter the tableData based on the input Value
   var filteredData = tableData.filter(ufo => ufo.datetime === inputValue);
-
+  
+  //Console log the filteredData
   console.log(filteredData);
 };
